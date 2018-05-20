8c0f353f0287e8d72117bfd1b4d8ec3553a46d61
Alex Arvedahl <alex.arvedahl@gmail.com>
2018-05-20 10:54:26 +0200

    Got react as a view engine

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -6,7 +7,13 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
+const http = require('http').Server(app);
+
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -14,7 +21,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//app.use('/users', usersRouter);
+/*
+app.get('/', function(req, res){
+    res.send('<h1>Hello world 123</h1>');
+});
+*/
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
 
 module.exports = app;
+
