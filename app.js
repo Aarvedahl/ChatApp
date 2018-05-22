b7f10f18d4d6e71058a4d0db947d272214bf3d32
Alex Arvedahl <alex.arvedahl@gmail.com>
2018-05-22 20:51:08 +0200

    User can send message

--- app.js
+++ app.js
@@ -31,7 +31,10 @@ http.listen(3000, function(){
 });
 
 io.on('connection', function(socket){
-    console.log('a user connected');
+    console.log('user connected');
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+    });
     socket.on('disconnect', function () {
         console.log('user disconnected');
     });
